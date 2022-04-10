f2dd408df33e1f89f7f972b3b7153e3d21bbacdc
Alex Wiles <alexandra.wiles6@gmail.com>
2022-04-10 15:55:58 -0400

    Added the table of contents section with appropriate tags for data entered into questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,64 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const userQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your Github Username (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your Username');
+                    return false;
+                }
+            }
+        }, 
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid email');
+                    return false;
+                }
+            }
+        }, 
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the projects Title (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid project title');
+                    return false;
+                }
+            }
+        }, 
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please enter a short description for the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a short description!');
+                    return false;
+                }
+            }
+        }, 
+
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,7 +4,14 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (licenseInput) {
+        return true;
+    } else {
+        console.log('Please enter a valid project title');
+        return false;
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,7 +20,34 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseSection(data.license)}
 
+  ## Table-of-Contents
+  * [Description] (#description)
+  * [Installation Instructions] (#installation)
+  * [License] (#license)
+  * [Usage Instructions] (#usage)
+  * [Test Instructions] (#tests)
+  * [Contributions] (#contribution)
+  
+  
+  ## Description
+  ${data.description}
+
+  ##Installation
+  ${data.installation}
+
+  ##License
+  This project is made by: ${data.license}
+
+  ##Usage Instructions
+  ${data.usage}
+
+  ##Test Instructions
+  ${data.test}
+
+  ##Contributions
+  ${data.contribution}
 `;
 }
 
